6c6e3edecf58028014626084a4b176165a869396
Galvin <galvincheung@gmail.com>
2020-07-04 14:57:54 -0600

    Updates

---
+++ precache-manifest.15dafe2b6d0efd22c1100af87fdf1ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b575dd66932b8599e9b2cef3a2eba5",
+    "url": "/quicknote/index.html"
+  },
+  {
+    "revision": "61ad17fefbfc59949745",
+    "url": "/quicknote/static/css/main.16a75160.chunk.css"
+  },
+  {
+    "revision": "8169162236ac42e61cf6",
+    "url": "/quicknote/static/js/2.cc8f0cbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quicknote/static/js/2.cc8f0cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ad17fefbfc59949745",
+    "url": "/quicknote/static/js/main.9d913a42.chunk.js"
+  },
+  {
+    "revision": "7dff56c7ec8715ea7c67",
+    "url": "/quicknote/static/js/runtime-main.57ae353e.js"
+  }
+]);
